0cd2f273a31ef5528a4ccc140e57965d78c1c792
BeatrizCSSantos <beatrizcs96@hotmail.com>
2021-09-12 20:33:08 -0300

    Pages modifications

---
+++ aulas/letmeask/src/pages/AdminRoom.tsx
@@ -0,0 +1,135 @@
+import toast from 'react-hot-toast'
+import { useHistory, useParams } from 'react-router-dom'
+import { Button } from '../components/Button'
+import { RoomCode } from '../components/RoomCode'
+
+import { Question } from '../components/Question'
+import { useRoom } from '../hooks/useRoom'
+import { database } from '../services/firebase'
+
+import deleteImg from '../assets/images/delete.svg'
+import logoImg from '../assets/images/logo.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
+import '../styles/room.scss'
+
+
+type RoomParams = {
+  id: string;
+}
+
+export function AdminRoom() {
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { questions, title } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
+      try {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        toast.success('Pergunta removida com sucesso');
+      } catch (err) {
+        toast.error('Não foi possível remover a pergunta');
+      }
+    }
+  }
+
+  async function handleEndRoom() {
+    try {
+      await database.ref(`rooms/${roomId}`).update({
+        endedAt: new Date()
+      });
+      toast.success('Sala foi encerrada');
+      history.push('/');
+    } catch (error) {
+      toast.error('Ops... não conseguimos encerrar sua sala no momento');
+    }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    try {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isAnswered: true
+      });
+      toast.success('Pergunta atualizada com sucesso');
+    } catch (err) {
+      toast.error('Não foi possível atualizar a pergunta');
+    }
+  }
+
+  async function handleHeighlightQuestion(questionId: string) {
+    try {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+        isHighlighted: true
+      });
+      toast.success('Pergunta destacada com sucesso');
+    } catch (err) {
+      toast.error('Não foi possível destacar a pergunta');
+    }
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="let me ask logo" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button onClick={handleEndRoom} isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+                isHighlighted={question.isHighlighted}
+                isAnswered={question.isAnswered}
+              >
+
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      title="Marcar como respondida"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+
+                    <button
+                      type="button"
+                      title="Destacar"
+                      onClick={() => handleHeighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
+
+                <button
+                  type="button"
+                  title="Remover"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
+            )
+          })}
+        </div>
+      </main>
+    </div>
+  )
+}
--- aulas/letmeask/src/pages/Home.tsx
+++ aulas/letmeask/src/pages/Home.tsx
@@ -1,34 +1,77 @@
+import { useHistory } from 'react-router';
+import toast from 'react-hot-toast';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
 
-export function Home(){
-    return(
-        <div>
-            <aside>
-                <img src={illustrationImg} alt="illustration symbolizing Questions and Answers" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiencia em tempo-real</p>
-                <main>
-                    <div>
-                        <img src={logoImg} alt="Letmeask" />
-                        <button>
-                            <img src={googleImg} alt="" />
-                            Crie sua sala com o Google
-                        </button>
-                        <div> ou entre em uma sala</div>
-                        <form>
-                            <input 
-                            type="text" 
-                            placeholder="Digite um código da sala"
-                            />
-                            <button type="submit">
-                                Entrar na sala
-                            </button>
-                        </form>
-                    </div>
-                </main>
-            </aside>
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+    if (!roomRef.exists()) {
+      toast.error('Essa sala não existe');
+      return;
+    }
+
+    if(roomRef.val().endedAt) {
+      toast.error('Essa sala foi encerrada');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo da LetMeAsk" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
